31dc8ad5c398c0f6fb88563413ca996a860ef08a
JialeiC <49631288+JialeiC@users.noreply.github.com>
2021-07-30 09:31:55 +0800

    Update views.py
    upload chapter8

--- chapter8/rango/views.py
+++ chapter8/rango/views.py
@@ -62,7 +62,6 @@ def add_page(request, category_name_slug):
     except:
         category = None
 
-    # You cannot add a page to a Category that does not exist... DM
     if category is None:
         return redirect(reverse('rango:index'))
 
